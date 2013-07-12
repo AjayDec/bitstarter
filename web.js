1ef6e585eee5537265296d864b49b64ad01c6895
Ubuntu <ubuntu@ip-172-31-22-100.us-west-2.compute.internal>
2013-07-12 19:36:37 +0000

    Updated to read file content

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var localFile = 'index.html';
+var getFileContent = function getFileConent(localFile) {
+  var buffer =fs.readFileSync(localFile);
+  return buffer.toString('utf-8');
+    
+};
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(getFileContent(localFile));
 });
 
 var port = process.env.PORT || 5000;
